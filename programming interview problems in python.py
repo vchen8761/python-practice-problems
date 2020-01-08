324cae50b9b0e3509310a3901e329db35af0ac47
Victor Chen <vchen8761@gmail.com>
2020-01-08 18:00:51 -0500

    worked on 1/8/20

--- programming interview problems in python.py
+++ programming interview problems in python.py
@@ -88,3 +88,55 @@
 
 # A = [-3, 0, -1, 1, 1, 2, 3, 1, 4, 2]
 # print(dutch_flag_partition(1,A))
+
+# 5.2 Increment an arbitrary-precision integer
+# def increment(A):
+#     carry = 0
+#     for i in range(len(A) - 1, -1, -1):
+#         if A[i] == 9:
+#             A[i] = 0
+#             carry = 1
+#             if i == 0:
+#                 A.insert(0,1)
+#         else:
+#             A[i] += 1
+#             return A
+#     return A
+
+# A = [1,2,9]
+# print(increment(A))
+
+# 5.5 Delete Duplicates
+# Naive Solution: O(n) extra space
+# def del_dupes(A):
+#     new_A = [A[0]]
+#     for i in range(1,len(A),1):
+#         if A[i] != new_A[len(new_A) - 1]:
+#             new_A.append(A[i])
+#     return len(new_A)
+
+# Ideal Solution: O(1) space, O(n) time
+# def del_dupes(A):
+#     if not A:
+#         return 0
+#     write_index = 1
+#     for i in range(1,len(A)):
+#         if A[write_index - 1] != A[i]:
+#             A[write_index] = A[i]
+#             write_index += 1
+#     return write_index
+
+# A = [2,3,5,5,7,11,11,11,13]
+# print(del_dupes(A))
+
+# 5.6 Maximum Stock Profit
+# def max_stock_profit(prices):
+#     min_price_so_far, max_profit = float('inf'), 0.0
+#     for price in prices:
+#         max_profit_sell_today = price - min_price_so_far
+#         max_profit = max(max_profit, max_profit_sell_today)
+#         min_price_so_far = min(min_price_so_far, price)
+#     return max_profit
+
+# A = [310,315,275,295,260,270,290,230,255,250]
+# print(max_stock_profit(A))
